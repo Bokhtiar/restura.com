a77e7943f7d063e1a5ef4c3ac0a0f09827cd10c3
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-03-15 15:37:32 +0600

    restura about section done

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,7 @@
 import { IButton } from '@/types/button.type'
 
 export const PrimaryButton: React.FC<IButton> = (props: IButton): JSX.Element => {
-    return <button className='flex items-center gap-3 bg-primary text-white py-2 px-4 rounded-br-3xl rounded-md'>
+    return <button className='flex items-center gap-3 bg-primary text-white py-2 px-4 rounded-br-3xl rounded-md mx-auto md:mx-0'>
         <span className='text-xl'>{props.name}</span>
         <span className="material-symbols-outlined mt-1">arrow_right_alt</span>
     </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,110 @@ import { PrimaryButton } from "@/components/button";
 import { Header } from "@/layouts/header";
 
 const Home: React.FC = (): JSX.Element => {
-  return <>
+  return (
+    <>
+      <Header></Header>
+      {/* banner section */}
+      <section className="bg-gray-50">
+        <div className="container grid grid-cols-1 md:grid-cols-2 items-center justify-between">
+          {/* content */}
+          <div>
+            {/* heading content */}
+            <p className="leading-10 pt-5">
+              <span className="md:text-5xl text-4xl font-bold text-blue-400">
+                Make Healthy Life
+              </span>
+              <br />
+              <span className="md:text-5xl text-4xl font-bold">
+                With Fitbitesmeal
+              </span>
+            </p>
+            {/* body content */}
+            <p className="leading-[0px] my-8">
+              <span className="md:text-3xl text-2xl text-gray-800 ">
+                Delicious meal the way you want,
+              </span>
+              <br />
+              <span className="md:text-3xl text-2xl text-gray-800">
+                When you want
+              </span>
+            </p>
+            {/* button */}
+            <PrimaryButton name="Order now" />
+          </div>
 
-  <Header></Header>
-    <section className="bg-gray-50">
-      <div className="container grid grid-cols-1 md:grid-cols-2 items-center justify-between">
-        {/* content */}
-        <div>
-          {/* heading content */}
-          <p className="leading-10 pt-5">
-            <span className="md:text-5xl text-4xl font-bold text-blue-400">Make  Healthy Life</span><br />
-            <span className="md:text-5xl text-4xl font-bold">With Fitbitesmeal</span>
-          </p>
-          {/* body content */}
-          <p className="leading-[0px] my-8">
-            <span className="md:text-3xl text-2xl text-gray-800 ">Delicious meal the way you want,</span> <br />
-            <span className="md:text-3xl text-2xl text-gray-800">When you want</span>
-          </p>
-          {/* button */}
-          <PrimaryButton name="Order now" />
+          {/* image */}
+          <Image
+            src="/images/banner1.png"
+            alt="banner1"
+            className="lg:w-[800px]"
+            width={550}
+            height={0}
+          />
         </div>
+      </section>
 
-        {/* image */}
+      {/* about us */}
+      <section className="container grid grid-cols-1 md:grid-cols-2 my-24">
+        {/* about image */}
         <Image
           src="/images/banner1.png"
           alt="banner1"
+          className="lg:w-[600px]"
           width={550}
           height={0}
         />
-      </div>
-    </section>
-  </>
+        {/* about content */}
+        <div>
+          {/* about heading content*/}
+          <h2 className="text-4xl text-center md:text-left md:text-6xl text-gray-700 font-bold">
+            <span className="pr-2">Healthy</span>
+            <span className=" bg-primary text-xl rounded-full text-primary">
+              O
+            </span>
+            <span className="px-2">Delicies</span>
+            <span className=" bg-primary text-xl rounded-full text-primary">
+              O
+            </span>
+            <br /> <span className="pr-2">Simple</span>
+            <span className=" bg-primary text-xl rounded-full text-primary">
+              O
+            </span>
+          </h2>
+
+          {/* about content  */}
+          <p className="my-16 text-gray-600 text-xl md:w-[550px] text-center md:text-left">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+
+          {/* happy client */}
+          <section className="my-16 grid grid-cols-1 md:grid-cols-3 gap-4 md:w-[550px]">
+            {/* member */}
+            <div className="leading-9 border md:border-none border-gray-400 px-4 py-2 rounded-md">
+              <h2 className="text-5xl font-bold text-primary">3k+</h2>
+              <span className="text-xl text-gray-700">Member active</span>
+            </div>
+
+            <div className="leading-9 border md:border-none border-gray-400 px-4 py-2 rounded-md">
+              <h2 className="text-5xl font-bold text-primary">8k+</h2>
+              <span className="text-xl text-gray-700">Happy client</span>
+            </div>
+
+            <div className="leading-9 border md:border-none border-gray-400 px-4 py-2 rounded-md">
+              <h2 className="text-5xl font-bold text-primary">20k+</h2>
+              <span className="text-xl text-gray-700">Award winning</span>
+            </div>
+          </section>
+
+          {/* button */}
+          <PrimaryButton name="Read more"></PrimaryButton>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
