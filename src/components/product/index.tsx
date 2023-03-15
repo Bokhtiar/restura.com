382d659663a44d80fdf943e2ca05b8738ecff5c5
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-03-15 16:55:47 +0600

    product deisng updated with responsive

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -8,22 +8,22 @@ export const Product: React.FC<IProduct> = (props: IProduct): JSX.Element => {
       <Image
         src="/images/banner1.png"
         alt="banner1"
-        className="mx-auto"
+        className="mx-auto py-2"
         width={250}
         height={0}
       />
       <div className="px-4">
         {/* content */}
         <h2 className="text-center text-2xl text-gray-600">{props.name}</h2>
-        <p className="text-[12px] leading-[14px] text-center text-gray-500 md:my-4">
+        <p className="text-[12px] text-justify leading-[14px]  text-gray-500 ">
           {props.short_description}
         </p>
         {/* footer section */}
-        <div className="my-4 px-4 text-gray-500 flex items-center justify-between ">
+        <div className="my-4 text-gray-500 flex items-center justify-between gap-4">
           <span className="text-primary material-symbols-outlined">
             favorite
           </span>
-          <span className="text-[16px]">{props.price} Tk</span>
+          <span className="text-[16px]">{props.price}Tk</span>
           <span className=" text-blue-500 material-symbols-outlined">
             shopping_cart
           </span>
--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -4,7 +4,6 @@ export const Title: React.FC<ITitle> = (props: ITitle): JSX.Element => {
   return (
     <>
       <h2 className="text-center text-6xl font-bold font-nunito text-primary">{props.name}</h2>
-      {/* <h2 className="text-center text-4xl font-bold font-nunito text-red-200  -mt-8 ">{props.name}</h2> */}
     </>
   );
 };
