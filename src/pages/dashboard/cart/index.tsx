2431d49b42f9c184073b4a76d1615fa5184b0ec1
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2023-03-17 11:40:22 +0600

    cart & order & wishlist responsive table

--- src/pages/dashboard/cart/index.tsx
+++ src/pages/dashboard/cart/index.tsx
@@ -2,7 +2,7 @@ import { BreadCrumbs } from "@/components/breadCrumbs"
 import { Sidebar } from "@/components/sidebar"
 import Link from "next/link"
 
-const Cart : React.FC = ():JSX.Element => {
+const Cart: React.FC = (): JSX.Element => {
     return <>
         <BreadCrumbs title1="Dashboard" title2="Cart" />
         <section className="container grid grid-cols-1 md:grid-cols-5 gap-4 md:h-[600px] bg-gray-50 p-6">
@@ -11,119 +11,43 @@ const Cart : React.FC = ():JSX.Element => {
                 <Sidebar />
             </div>
             {/* content */}
-            <div className="col-span-4">
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="px-6 py-3">
-                                    Product name
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Color
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Category
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Price
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Action
-                                </th>
+            <div className="md:col-span-4">
+                <div className="shadow-lg overflow-x-auto">
+                    <table className="w-full">
+                        <thead className="text-left bg-gray-300">
+                            <tr className=" border border-solid">
+                                <th className="text-md px-6 py-4">index</th>
+                                <th className="text-md px-6 py-4">index</th>
+                                <th className="text-md px-6 py-4">index</th>
+                                <th className="text-md px-6 py-4">index</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple MacBook Pro 17"
-                                </th>
-                                <td className="px-6 py-4">
-                                    Silver
-                                </td>
-                                <td className="px-6 py-4">
-                                    Laptop
-                                </td>
-                                <td className="px-6 py-4">
-                                    2999
-                                </td>
-                                <td className="px-6 py-4">
-                                    <Link href="/" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                </td>
-                            </tr>
-                            <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Microsoft Surface Pro
-                                </th>
-                                <td className="px-6 py-4">
-                                    White
-                                </td>
-                                <td className="px-6 py-4">
-                                    Laptop PC
-                                </td>
-                                <td className="px-6 py-4">
-                                    1999
-                                </td>
-                                <td className="px-6 py-4">
-                                    <Link href="/" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                </td>
-                            </tr>
-                            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Magic Mouse 2
-                                </th>
-                                <td className="px-6 py-4">
-                                    Black
-                                </td>
-                                <td className="px-6 py-4">
-                                    Accessories
-                                </td>
-                                <td className="px-6 py-4">
-                                    99
-                                </td>
-                                <td className="px-6 py-4">
-                                    <Link href="/" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                </td>
-                            </tr>
-                            <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Google Pixel Phone
-                                </th>
-                                <td className="px-6 py-4">
-                                    Gray
-                                </td>
-                                <td className="px-6 py-4">
-                                    Phone
-                                </td>
-                                <td className="px-6 py-4">
-                                    799
-                                </td>
-                                <td className="px-6 py-4">
-                                    <Link href="/" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                </td>
-                            </tr>
                             <tr>
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple Watch 5
-                                </th>
-                                <td className="px-6 py-4">
-                                    Red
-                                </td>
-                                <td className="px-6 py-4">
-                                    Wearables
-                                </td>
-                                <td className="px-6 py-4">
-                                    999
-                                </td>
-                                <td className="px-6 py-4">
-                                    <Link href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</Link>
-                                </td>
+                                <td className="text-md px-6 py-4">dddd</td>
+                                <td className="text-md px-6 py-4">dddd</td>
+                                <td className="text-md px-6 py-4">dddd</td>
+                                <td className="text-md px-6 py-4">dddd</td>
+                            </tr>
+
+                            <tr>
+                                <td className="text-md px-6 py-4">dddd</td>
+                                <td className="text-md px-6 py-4">dddd</td>
+                                <td className="text-md px-6 py-4">dddd</td>
+                                <td className="text-md px-6 py-4">dddd</td>
+                            </tr>
+
+                            <tr>
+                                <td className="text-md px-6 py-4">dddasdfasd asdd</td>
+                                <td className="text-md px-6 py-4">dddasdfa sdasdfas dasdfasfdd</td>
+                                <td className="text-md px-6 py-4">dd asdf asdfa sdasfadsfasdfsdfdd</td>
+                                <td className="text-md px-6 py-4">dddd</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
-
             </div>
+
         </section>
     </>
 }
