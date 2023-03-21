007109499fbb2c5324d42c0770bf6feec5c7396c
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-03-21 15:30:29 +0600

    category sidebar desing updated

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,12 +1,13 @@
 import { BreadCrumbs } from "@/components/breadCrumbs";
 import { Product } from "@/components/product";
 import { Title } from "@/components/title";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
 const Menu: React.FC = (): JSX.Element => {
   const [category, setCategory] = useState<boolean>(false);
-  const [col, setCol] = useState<number>(0)
+  const [col, setCol] = useState<number>(0);
 
   return (
     <>
@@ -17,15 +18,131 @@ const Menu: React.FC = (): JSX.Element => {
         <div className=" col-span-1 shadow-lg p-4">
           <span className="text-gray-600 text-md">All Category</span>
           <ul>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
-            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2"> <Link href="" className="">Pizza</Link> </li>
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
 
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
+
+
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
+
+
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
+
+
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
+
+
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
+
+
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
+
+
+            <li className="py-2 w-full border  px-3 rounded-md text-gray-500 hover:bg-primary hover:text-white my-2 flex justify-between gap-2">
+              <div className="flex gap-2">
+              <Image
+                src="/images/menu-item-1.png"
+                alt="Picture of the author"
+                width={25}
+                height={20}
+              />
+              <Link href="" className="">
+                Pizza
+              </Link>
+              </div>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </li>
           </ul>
         </div>
 
@@ -34,21 +151,36 @@ const Menu: React.FC = (): JSX.Element => {
           <div className="">
             {/* menus */}
             <div className="flex items-center justify-between mb-4">
-              <p className="p-1 text-gray-500 border border-primary rounded-t-sm px-6 cursor-pointer hover:bg-primary hover:text-white" onClick={()=> setCol(0)}>Defult items</p>
+              <p
+                className="p-1 text-gray-500 border border-primary rounded-t-sm px-6 cursor-pointer hover:bg-primary hover:text-white"
+                onClick={() => setCol(0)}
+              >
+                Defult items
+              </p>
 
               {/* right side icon */}
               <div className="flex gap-2 items-center">
-                <span className="border border-primary text-gray-500 rounded-sm material-symbols-outlined cursor-pointer hover:bg-primary hover:text-white" onClick={()=> setCol(3)}>
+                <span
+                  className="border border-primary text-gray-500 rounded-sm material-symbols-outlined cursor-pointer hover:bg-primary hover:text-white"
+                  onClick={() => setCol(3)}
+                >
                   list
                 </span>
-                <span className=" border border-primary text-gray-500 rounded-sm material-symbols-outlined cursor-pointer hover:bg-primary hover:text-white" onClick={()=> setCol(5)}>
+                <span
+                  className=" border border-primary text-gray-500 rounded-sm material-symbols-outlined cursor-pointer hover:bg-primary hover:text-white"
+                  onClick={() => setCol(5)}
+                >
                   view_cozy
                 </span>
               </div>
             </div>
 
             {/* product */}
-            <div className={`grid grid-cols-2 ${col ? `md:grid-cols-${col}` : 'md:grid-cols-4' } ${col ? `xxl:grid-cols-${col}` : ' xxl:grid-cols-5' }  gap-4`}>
+            <div
+              className={`grid grid-cols-2 ${
+                col ? `md:grid-cols-${col}` : "md:grid-cols-4"
+              } ${col ? `xxl:grid-cols-${col}` : " xxl:grid-cols-5"}  gap-4`}
+            >
               <Product
                 name="Chiken carry"
                 price={20}
